534f2541b1590ba87c38680018d70a8154dbee58
Abdullah <141170550+AbdullahGM1@users.noreply.github.com>
2024-12-02 11:52:44 +0300

    update

--- src/depth_map_detection_localization.cpp
+++ src/depth_map_detection_localization.cpp
@@ -89,7 +89,7 @@ private:
         pcl::PointCloud<pcl::PointXYZ>::Ptr filtered_cloud(new pcl::PointCloud<pcl::PointXYZ>);
         pcl::PassThrough<pcl::PointXYZ> pass;
         pass.setInputCloud(pcl_cloud);
-        pass.setFilterFieldName("x");
+        pass.setFilterFieldName("z");
         pass.setFilterLimits(MinDepth, MaxDepth);
         pass.filter(*filtered_cloud);
 
